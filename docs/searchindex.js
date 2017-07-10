fec468189aa699e6858c759c85eb72adfc42b86e
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2017-07-10 03:18:56 -0300

    [api-doc] add sphinx build files

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to azion-python-sdk\u2019s documentation!"],titleterms:{azion:0,document:0,indic:0,python:0,sdk:0,tabl:0,welcom:0}})
